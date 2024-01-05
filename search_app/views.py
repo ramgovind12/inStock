e012413594dfb4e24b56de14c8ec801903189e57
Ram Govind V <ramgovindvi.in@gmail.com>
2024-01-05 16:31:21 +0530

    final changes

--- search_app/views.py
+++ search_app/views.py
@@ -9,5 +9,5 @@ def searchResult(request):
 
     if 'q' in request.GET:
         query = request.GET.get('q')
-        products = Product.objects.all().filter(Q(name__contains=query) | Q(description__contains=query))
-        return render(request,'search.html',{'query':query,'products':products})
+        products = Product.objects.filter(Q(name__icontains=query) | Q(description__icontains=query))
+        return render(request, 'search.html', {'query': query, 'products': products})
